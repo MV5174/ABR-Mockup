5f16b1e1b295bd7be0b3b2fe948efd7c75436f14
Michael Vega <ichigo509@yahoo.com>
2020-08-20 02:43:01 -0400

    fixing header

--- abr-mockup/src/Components/Header/Header.js
+++ abr-mockup/src/Components/Header/Header.js
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
 import "./Header.css";
+import { Button } from 'react-bootstrap';
 
 class Header extends Component {
     render() {
         return (
-            <div className="header"><h1>This is the Header</h1></div>
+            <div className="header"><h1>This is the Header</h1>
+            
+            <Button variant="warning">Warning</Button>
+            
+            </div>
         )
     }
 }
